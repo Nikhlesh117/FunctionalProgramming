aa3789480d6160d72e4a7349a85699f1192b71bd
“Nikhlesh117” <nikhlesh.rao@gmail.com>
2023-02-14 09:07:07 +0530

    ADDED: VowelAndConsonant

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace LogicalProgramming
+{
+    public class program
+    {
+        public static void Main(String[] args)
+        {
+            VowelAndConsonant.CheckTheCharacter();
+            //Swap.SwapingTheNumber();
+            //LargestNumber.FindTheLargestNumber();
+            //QuotientAndRemonder.FindTheQuotientAndRemonder();
+            //EvenOrOdd.FindEvenOrOdd();
+
+            //FlipCoin.PercentCoin();
+            //LeapYear.Year();
+            //PowerOf2.FindPowerOf2();
+            //HarmonicNumber.FindHarmonicNumber();
+            //Factor.FindFactor();
+
+        }
+    }
+}
